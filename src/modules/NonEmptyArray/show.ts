9d0c0796be31a72c2646724cb05cd1e46c0d0ba4
Lothering0 <danil2005232@gmail.com>
2025-07-08 23:45:42 +0300

    Checkpoint

--- src/modules/NonEmptyArray/show.ts
+++ src/modules/NonEmptyArray/show.ts
@@ -1,11 +1,7 @@
+import * as A from "../Array"
 import { NonEmptyArray } from "./non-empty-array"
-import { join } from "../Array"
 import { Show } from "../../types/Show"
-import { flow } from "../../utils/flow"
-import { map } from "./functor"
 
 export const getShow: {
   <A>(S: Show<A>): Show<NonEmptyArray<A>>
-} = S => ({
-  show: flow (map (S.show), join (", "), x => `[${x}]`),
-})
+} = A.getShow
