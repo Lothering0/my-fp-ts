1ba432fab10bfe56288f58c0d4f51843d0e2576a
Lothering0 <danil2005232@gmail.com>
2025-08-24 14:56:47 +0300

    Checkpoint

--- src/modules/Result/alt.ts
+++ src/modules/Result/alt.ts
@@ -19,21 +19,6 @@ export const catchAll: {
     onFailure: (e: E1) => Result<E2, B>,
   ): <A>(self: Result<E1, A>) => Result<E2, A | B>
 } = onFailure => match (onFailure, succeed)
-/* export const getOrElse: {
-  <E, B>(onFailure: (e: E) => B): <A>(self: Result<A, E>) => A | B
-} = onFailure => match (onFailure, identity)
-
-export const orElse: {
-  <A, E1>(
-    onFailure: Result<A, E1>,
-  ): <B, E2>(self: Result<B, E2>) => Result<A | B, E1 | E2>
-} = onFailure => match (constant (onFailure), succeed)
-
-export const catchAll: {
-  <B, E1, E2>(
-    onFailure: (e: E1) => Result<B, E2>,
-  ): <A>(self: Result<A, E1>) => Result<A | B, E2>
-} = onFailure => match (onFailure, succeed) */
 
 export const Alt: alt.Alt<ResultHkt> = {
   orElse,
--- src/modules/Result/result.ts
+++ src/modules/Result/result.ts
@@ -29,32 +29,3 @@ export const succeed: {
   _tag: "Success",
   success: value,
 })
-/* export interface ResultHkt extends Hkt {
-  readonly type: Result<this["_in"], this["_collectable"]>
-}
-
-export type Result<A, E = never> = Success<A> | Failure<E>
-
-export interface Failure<E> {
-  readonly _tag: "Failure"
-  readonly failure: E
-}
-
-export interface Success<A> {
-  readonly _tag: "Success"
-  readonly success: A
-}
-
-export const fail: {
-  <E>(e: E): Result<never, E>
-} = value => ({
-  _tag: "Failure",
-  failure: value,
-})
-
-export const succeed: {
-  <A>(a: A): Result<A>
-} = value => ({
-  _tag: "Success",
-  success: value,
-}) */
