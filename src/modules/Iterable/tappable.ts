2306e8510edc69040af6aba35ef37e66fd227e21
Lothering0 <danil2005232@gmail.com>
2025-09-12 21:41:45 +0300

    Checkpoint

---
+++ src/modules/Iterable/tappable.ts
@@ -0,0 +1,13 @@
+import { createTappable } from "../../typeclasses/Tappable"
+import { Sync } from "../Sync"
+import { Monad } from "./monad"
+
+export const Tappable = createTappable (Monad)
+
+export const tap: {
+  <A>(f: (a: A) => Iterable<unknown>): (self: Iterable<A>) => Iterable<A>
+} = Tappable.tap
+
+export const tapSync: {
+  <A>(f: (a: A) => Sync<unknown>): (self: Iterable<A>) => Iterable<A>
+} = Tappable.tapSync
